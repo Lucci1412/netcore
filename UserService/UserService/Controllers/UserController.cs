d10978360bc22a0f6b1ec87a28b74788c9b70cee
Lucci1412 <66712654+Lucci1412@users.noreply.github.com>
2024-05-07 21:44:18 +0700

    create user api

---
+++ UserService/UserService/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Models;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        public static List<User> users = new List<User>();
+
+        [HttpGet]
+        public  IActionResult GetAllUser()
+        {
+            return Ok( 
+               users
+            );
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateUser( User model)
+        {
+            var user = new User { 
+            Name = model.Name  ,
+            Phone = model.Phone,
+            Password=model.Password,
+            IsDelete = model.IsDelete
+           
+            };
+            users.Add(user);
+            return Ok(new
+            {
+                Success= true,
+                Data=user
+                
+            });
+        }
+    }
+}
